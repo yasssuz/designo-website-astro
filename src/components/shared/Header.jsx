aa601c6092c74b652172cf56f56753296b414cb8
Karim Yassine <karimwebdevelopment@gmail.com>
2021-08-22 17:15:09 -0300

    transformed astro file to react component

---
+++ src/components/shared/Header.jsx
@@ -0,0 +1,32 @@
+import { useState, useEffect } from "react";
+
+export default function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  useEffect(() => {
+    console.log(menuOpen);
+  }, [menuOpen]);
+
+  return (
+    <header className='header'>
+      <a className='header__logo' href='/'>
+        <img src='/assets/logo.svg' alt='Designo' />
+      </a>
+      <button
+        className='header__toggler'
+        onClick={() => setMenuOpen(prevState => !prevState)}
+      >
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+      <nav className={`header__navigation ${menuOpen && "active"}`}>
+        {["about", "locations", "contact"].map(item => (
+          <a key={item} href={`./${item}`}>
+            {item}
+          </a>
+        ))}
+      </nav>
+    </header>
+  );
+}
